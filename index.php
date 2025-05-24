8fd7d52744bce2adc72a96b3616a4914bc0b658b
fellfire <r1der88@mail.ru>
2025-05-24 14:25:25 +0300

    comments

--- index.php
+++ index.php
@@ -9,25 +9,27 @@ ini_set('error_reporting', E_ALL);
 ini_set('log_errors', 1);
 
 try {
+    //Подключение зависимостей src/Root
     require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
+    //Ввод настроек подключения к БД
     DB::init(DB::DB_POSGRES, 'localhost', 'postgres', 'postgres', '', DB::CHARSET_NULL);
 
+    //Парсинг url
     $url = explode('/', $_SERVER['REQUEST_URI']);
 
     switch (count($url)) {
-        case 1:
-            require_once 'pages/main/index.php';
-            break;
         case 2:
             switch ($url[1]) {
+                // url-адрес вида "mydomain/"
                 case '':
                     require_once 'pages/main/index.php';
                     break;
+                // url-адрес для ajax-запроса вида "mydomain/url"
                 case 'url':
                     require_once 'pages/ajax/index.php';
                     break;
-
+                // url-адрес для переадресации вида "mydomain/hashFromDomain"
                 default:
                     die(header("Location: " . URL::getURL($url[1])));
                     break;
@@ -39,6 +41,6 @@ try {
             break;
     }
 } catch (Exception $e) {
-    echo $e->getMessage();
+    // переход на страницу 404
     require_once 'pages/empty/index.php';
 }
--- pages/ajax/index.php
+++ pages/ajax/index.php
@@ -3,6 +3,7 @@
 use Api\Root\URL;
 
 try {
+    //принимаем и проверяем url
     if (!isset($_POST['url'])) {
         throw new Exception('Empty URL');
     }
@@ -13,11 +14,14 @@ try {
         throw new Exception('Invalid URL');
     }
 
+    //проверяем наличие url в БД, при отсутствии создаем short для этого url
     if (!URL::test($url)) {
         URL::make($url);
     }
 
+    //отправляем short, если нет ошибок
     echo json_encode(['OK', URL::getShort()]);
 } catch (Exception $e) {
+    //отправляем ошибку с текстом
     echo json_encode(['error', $e->getMessage()]);
 }
